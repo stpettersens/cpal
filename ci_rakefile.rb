2708381825ce24e9951f68592f05cb0ec6781fbe
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2017-06-24 17:50:56 +0100

    Configuration->Config and one liner.

--- ci_rakefile.rb
+++ ci_rakefile.rb
@@ -4,11 +4,10 @@
 #
 require 'json'
 
-Configuration = Struct.new(:ssid, :portal, :field_user, :field_pwd, 
-:username, :password, :auto_login, :wifi_mode)
+Config = Struct.new(:ssid, :portal, :field_user, :field_pwd, :username, :password, :auto_login, :wifi_mode)
 
-task :data do #[ignore]
-    config = Configuration.new("DUMMY_ID", "127.0.0.1:2015/login.php", 
+task :data do
+    config = Config.new("DUMMY_ID", "127.0.0.1:2015/login.php", 
     "bs_username", "bs_password", "root", "toor", 1, 1)
     json = File.open(".cpal.json", "w")
     json.write(config.to_h.to_json)
