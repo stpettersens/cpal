aea905d2400ab47baa350532e49c30b763688b52
Sam Saint-Pettersen <s.stpettersen+github@gmail.com>
2017-06-25 21:55:12 +0100

    +n

--- ci_rakefile.rb
+++ ci_rakefile.rb
@@ -1,14 +1,13 @@
 #
 # Rakefile to create a dummy configuration file for `cpal` 
-# in a CI enviroment such as Travis CI.
+# in a CI environment such as Travis CI.
 #
 require 'json'
 
 Config = Struct.new(:ssid, :portal, :field_user, :field_pwd, :username, :password, :auto_login, :wifi_mode)
 
 task :data do
-    config = Config.new("DUMMY_ID", "127.0.0.1:2015/login.php", 
-    "bs_username", "bs_password", "root", "toor", 1, 1)
+    config = Config.new("DUMMY_ID", "127.0.0.1:2015/login.php", "bs_username", "bs_password", "root", "toor", 1, 1)
     json = File.open(".cpal.json", "w")
     json.write(config.to_h.to_json)
     puts config.to_h.to_json
